f83124c43ab97828c2eb70cc1f7adfff2d557331
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-08 11:21:22 +0530

    Update and rename README.md to ascii.java

---
+++ ascii.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch = 'R';
+    int ascii = ch;
+    int castAscii = (int) ch;
+    System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+	
+	}
+}
